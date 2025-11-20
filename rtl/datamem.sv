0818ba0aea21c023a7f189f4cfb86445d9ada6d1
louis574 <loujamcan@btinternet.com>
2025-11-20 18:34:33 +0000

    datamemdone

--- rtl/datamem.sv
+++ rtl/datamem.sv
@@ -1,9 +1,17 @@
-module datamem #()(
+typedef enum logic [1:0] {
+    b = 2'b00,
+    half = 2'b01,
+    word = 2'b11
+} rw_type;
+
+module datamem #(parameter mem_size = 512)(
     input clk,
     input write_en,
 
-    input logic [4:0] write_addr,
-    input logic [4:0] read_addr,
+    input rw_type type_control,
+
+    input logic [31:0] write_addr,
+    input logic [31:0] read_addr,
 
     input logic [31:0] din,
 
@@ -12,20 +20,58 @@ module datamem #()(
 );
 
 
- logic [7:0] register [31:0];
+ logic [7:0] memory [mem_size - 1:0];
 
 
 always_ff @(posedge clk) begin
 
-    else if(write_en) begin
-    
-            //different writes
-    
+    if(write_en) begin
+        case(type_control)
+        
+            b: memory[write_addr] <= din[7:0];
+            
+            half: begin
+                memory[write_addr] <= din[7:0];
+                memory[write_addr+1] <= din[15:8];
+            end
+            
+            word: begin
+                memory[write_addr] <= din[7:0];
+                memory[write_addr+1] <= din[15:8];
+                memory[write_addr+2] <= din[23:16];
+                memory[write_addr+3] <= din[31:24];
+            end
+        endcase
     end
 
 
 end
 
-            //different reads
+
+always_comb begin
+
+    case(type_control)
+    
+        b: dout = {24'b0,memory[read_addr]};
+        
+        half: begin
+            
+            dout = {16'b0,memory[read_addr+1],memory[read_addr]};
+        end
+        
+        word: begin
+            dout = {memory[read_addr+3],memory[read_addr+2],memory[read_addr+1],memory[read_addr]};
+            
+            
+            
+        end
+    endcase
+
+
+
+
+end
+
+
 
 endmodule
