7e6b55f1d9ca81e779cba3783e78b7f22bdf5ed7
SAMOS-76 <sosebeyo@gmail.com>
2025-11-21 14:29:33 +0000

    Top Level components

---
+++ rtl/mux4.sv
@@ -0,0 +1,23 @@
+module mux4 #(
+    DATA_WIDTH = 32
+) (
+    input   logic [DATA_WIDTH-1:0]  in0,
+    input   logic [DATA_WIDTH-1:0]  in1,
+    input   logic [DATA_WIDTH-1:0]  in2,
+    input   logic [DATA_WIDTH-1:0]  in4,
+    input   logic [1:0]             sel,
+    output  logic [DATA_WIDTH-1:0]  out
+);
+
+    always_comb begin
+        case (sel)
+           2'b00: out = in0; 
+           2'b01: out = in0; 
+           2'b10: out = in0; 
+           2'b11: out = in0; 
+           default: out = in0;
+
+        endcase
+    end
+
+endmodule
---
+++ rtl/pc_reg.sv
@@ -0,0 +1,24 @@
+module pc_reg #(
+    DATA_WIDTH = 32
+) (
+    input logic clk,
+    input logic rst,
+
+    input logic [DATA_WIDTH-1: 0] pc_next,
+    output logic [DATA_WIDTH-1: 0] pc_out
+);
+
+    logic [DATA_WIDTH:0] PC;
+
+    always_ff @(posedge clk, posedge rst)
+        if (rst) begin
+            PC <= 32'b0;
+        end
+
+        else begin
+            PC <= pc_next;
+        end
+    
+    assign pc_out = PC;
+    
+endmodule
