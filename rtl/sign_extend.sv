32e0b11d89e18a34734cd43fd7f71d03a8cdd19a
Archie <archie.kendall23@imperial.ac.uk>
2025-11-24 19:13:40 +0000

    Revert "U-type / j-type encoding wrong way round fixed"

--- rtl/sign_extend.sv
+++ rtl/sign_extend.sv
@@ -1,6 +1,3 @@
-/* verilator lint_off UNUSED */
-//bits 6->0 unused
-
 module sign_extend (
     input logic [2:0] immSrc,
     input logic [31:0] instr,
@@ -11,9 +8,9 @@ always_comb begin
     case (immSrc)
         3'b000: imm_ext = {{20{instr[31]}}, instr[31:20]};        
         3'b001: imm_ext = {{20{instr[31]}}, instr[31:25], instr[11:7]};                                  
-        3'b010: imm_ext = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};                                              
-        3'b011: imm_ext = {{11{instr[31]}}, instr[31], instr[19:12], instr[20], instr[30:21], 1'b0}; 
-        3'b100: imm_ext = {instr[31:12], 12'b0};                          
+        3'b010: imm_ext = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};                           
+        3'b011: imm_ext = {instr[31:12], 12'b0};                        
+        3'b100: imm_ext = {{11{instr[31]}}, instr[31], instr[19:12], instr[20], instr[30:21], 1'b0};                           
         default: imm_ext = 32'b0;
     endcase
 end
