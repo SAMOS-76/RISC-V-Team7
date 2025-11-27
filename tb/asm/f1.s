d1998e446806f2082b24badcfc069df31cd8bf36
louis574 <loujamcan@btinternet.com>
2025-11-27 16:04:21 +0000

    f1 assembly source v1

---
+++ tb/asm/f1.s
@@ -0,0 +1,25 @@
+main:
+    li      a0, 0   # output register: a0 
+    li      t0, 0   # iteration counter: t0
+
+    li      t1, 8   # iteration number
+
+loop:   #loops until t0 = 8 (light sequence complete)
+
+    bge     to, t1, lights_out    
+    jal     ra, increase       
+    addi    t0, t0, 1
+    j       loop
+
+increase: #adds another light
+
+    slli    t2, a0, 1
+    addi    a0, t2, 1
+    RET     #returning to loop
+
+lights_out:
+    li      a0, 0
+
+
+end:
+    j     end
